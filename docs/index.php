b28a63c567e697e1203d2a3f14a571d021fcf2e7
Lelenn85 <65108715+Lelenn85@users.noreply.github.com>
2021-04-16 21:45:24 +0200

    Create index.php

---
+++ docs/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>R.I.C.I</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css" 
+    </head>
+  <body>
+    
+
+
+<main class="container">
+  <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+    <div class="col-md-6 px-0">
+      <h1 class="display-4 fst-italic"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Présentation</font></font></h1>
+      <p class="lead my-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Le projet R.I.C.I est un acronyme du nom complet de ce projet : Robot Intelligent Commandé par Infra-rouge.</font></font></p>
+      <p class="lead mb-0"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Continuer la lecture...</font></font></p>
+    </div>
+  </div>
+
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Modélisation</font></font></strong>
+          <div class="mb-1 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">XX XX XX</font></font></div>
+          <p class="card-text mb-auto"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Conception de A à Z du chassis du robot R.I.C.I avec le logiciel SOLIDWORKS.</font></font></p>
+          <a href="#" class="stretched-link"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">continuer la lecture</font></font></a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Espace réservé: miniature" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Capture</text></svg>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Programmation</font></font></strong>
+          <div class="mb-1 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">XX XX XX</font></font></div>
+          <p class="mb-auto"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Conception du programme de A à Z avec des algorigrammes au code final.</font></font></p>
+          <a href="#" class="stretched-link"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">continuer la lecture</font></font></a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Espace réservé: miniature" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Capture</text></svg>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <div class="col-md-4">
+      <div class="p-4 mb-3 bg-light rounded">
+        <h4 class="fst-italic"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos de</font></font></h4>
+        <p class="mb-0"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Je t'ai vu chanter les Blues au centre-ville. </font><font style="vertical-align: inherit;">Regardez-vous faire le tour du drain. </font><font style="vertical-align: inherit;">Pourquoi ne me laissez-vous pas passer? </font><font style="vertical-align: inherit;">Lourd est la tête qui </font></font><em><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">porte la couronne</font></font></em><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> . </font><font style="vertical-align: inherit;">Oui, nous faisons pleurer les anges, pleuvant sur la terre d'en haut.</font></font></p>
+        </div>
+      <div class="p-4">
+        <h4 class="fst-italic"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ailleurs</font></font></h4>
+        <ol class="list-unstyled">
+          <li><a href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">GitHub</font></font></a></li>
+          <li><a href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">SolidWorks</font></font></a></li>
+          <li><a href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Arduino</font></font></a></li>
+            <li><a href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">TinkerCad</font></font></a></li>
+            <li><a href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">BootStrap</font></font></a></li>
+        </ol>
+      </div>
+    </div>
+
+  </div><!-- /.row -->
+
+</main><!-- /.container -->
